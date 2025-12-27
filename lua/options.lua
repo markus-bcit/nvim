70f9d9407d3cd551226b84afa99b118b1e3daf72
markus-bcit <markusafonso1@gmail.com>
2025-12-26 18:31:32 -0800

    Add relative line numbers

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,8 @@ require "nvchad.options"
 
 -- add yours here!
 
+vim.opt.relativenumber = true  -- relative line numbers
+vim.opt.number = true           -- show current line number (not relative)
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
