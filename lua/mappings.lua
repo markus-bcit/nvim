1ac803fb16268c2b5360c61c63460faa653a534f
markus-bcit <markusafonso1@gmail.com>
2026-01-15 13:36:56 -0800

    Add sql shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,5 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Added 
+vim.keymap.set({"n", "v"}, "<leader>rq", ":DB mysql://markus:P%40ssw0rd@127.0.0.1:3306<CR>", { desc = "Run Query on MySQL" })
