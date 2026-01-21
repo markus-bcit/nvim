a8826e61433b18fc6bf140587da9d2fa40262ac9
markus-bcit <markusafonso1@gmail.com>
2026-01-20 21:47:22 -0800

    Update folding in DBUI

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,9 @@
 require "nvchad.autocmds"
+
+-- Disable folding in Dadbod UI output
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dbout",
+  callback = function()
+    vim.opt_local.foldenable = false
+  end,
+})
