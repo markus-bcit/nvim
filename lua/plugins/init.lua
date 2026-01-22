77507c8b9fed8cc1b23f13bb8501b6975d850d07
markus-bcit <markusafonso1@gmail.com>
2026-01-22 10:02:39 -0800

    Add sql to treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,15 +16,16 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+    lazy = false,
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "sql"
+  		},
+  	},
+  },
 
   {
     "ThePrimeagen/vim-be-good",
