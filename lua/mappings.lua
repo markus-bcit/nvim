a08164081b50602174cf490a2b0bba2d43af49dc
markus-bcit <markusafonso1@gmail.com>
2026-01-22 14:28:32 -0800

    Update sql mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,4 +9,4 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- Added 
-vim.keymap.set({"n", "v"}, "<leader>rq", ":DB mysql://markus:P%40ssw0rd@127.0.0.1:3306<CR>", { desc = "Run Query on MySQL" })
+vim.keymap.set({"n", "v"}, "<leader>rq", ":DB mysql://127.0.0.1:3306<CR>", { desc = "Run Query on MySQL" })
