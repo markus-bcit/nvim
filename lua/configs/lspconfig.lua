61fe3cc298f0fd52ce4f33fefe6e2b34bee294e4
markus-bcit <markusafonso1@gmail.com>
2026-02-04 13:22:25 -0800

    Update pyright settings

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local nvlsp = require "nvchad.configs.lspconfig"
 -- load defaults i.e lua_lsp
 nvlsp.defaults()
 
-local servers = { "html", "cssls", "pyright", "clangd" }
+local servers = { "html", "cssls", "csharp_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -14,3 +14,21 @@ for _, lsp in ipairs(servers) do
   }
   vim.lsp.enable(lsp)
 end
+
+-- Pyright specific config for better type inference
+vim.lsp.config.pyright = {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "workspace",
+        typeCheckingMode = "basic", -- options: "off", "basic", "strict"
+      },
+    },
+  },
+}
+vim.lsp.enable("pyright")
